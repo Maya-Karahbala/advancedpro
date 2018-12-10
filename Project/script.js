2268b20355f8e5675f567abd9afe3adcfde76f1a
Unknown <maya.karabala8@gmail.com>
2018-12-10 16:44:12 +0300

    proje

--- Project/script.js
+++ Project/script.js
@@ -1,17 +1,8 @@
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");	
-canvas.width = 1131;
-canvas.height = 700;
-// show x and y in consoly when you press in canvas useful for drawing
-canvas.onmousedown=function(e){
-console.log("x :"+e.layerX+" y :"+e.layerY)
-
-
+var boxes=[];
+let boxName;
+for (let i = 0; i <4; i++) {
+	boxName="box"+i
+	boxes.push(document.getElementById(boxName))
+	boxes[i].position=i
 }
-var img=new Image();
-img.src="pictures/bg2.jpg"
-window.onload = function() {
-   ctx.drawImage(img,0,200,1131,700,0,0,1131,700);
-    
-};
